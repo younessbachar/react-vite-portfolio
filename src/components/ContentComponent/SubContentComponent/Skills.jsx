6fc9bebfccae32b1ca36d9bac93fed6307e27c9f
youness bachar khir <81125054+younessbachar@users.noreply.github.com>
2025-08-25 14:13:52 +0100

    small edit

--- src/components/ContentComponent/SubContentComponent/Skills.jsx
+++ src/components/ContentComponent/SubContentComponent/Skills.jsx
@@ -6,37 +6,43 @@ const Skills = () => {
       icon: "fa-brands fa-react",
       title: "Front-End Development",
       description: "Responsive and optimized web applications with modern frameworks.",
-      subicons: "subicont"
+      subicons: ["fa-brands fa-html5", "fa-brands fa-css3-alt", "fa-brands fa-js", "fa-brands fa-react"],
+      SubiconsName: ["HTML5", "CSS3", "JavaScript", "React"]
     },
     {
       icon: "fa-solid fa-code",
       title: "Styling & UI Frameworks",
       description: "modern and maintainable UI components with efficient styling techniques.",
-      subicons: "icon"
+      subicons: ["fa-brands fa-tailwind", "fa-brands fa-bootstrap", "fa-brands fa-css3-alt", "fa-solid fa-code"],
+      SubiconsName: ["TailwindCSS", "Bootstrap", "CSS Modules", "ShadCN"]
     },
     {
       icon: "fa-solid fa-cubes",
       title: "State Management",
       description: "Handling application state efficiently for scalable applications.",
-      subicons: "icon"
+      subicons: ["fa-brands fa-react", "fa-brands fa-ne"],
+      SubiconsName: ["Context API", "Redux"]
     },
     {
       icon: "fa-solid fa-terminal",
       title: "Back-End Integration",
       description: "Connecting front-end applications to databases and back-end services.",
-      subicons: "icon"
+      subicons: ["fa-brands fa-mongodb","fa-brands fa-node-js", "fa-solid fa-code"],
+      SubiconsName: ["MongoDB","Node.js", "REST APIs"]
     },
     {
       icon: "fa-solid fa-wrench",
       title: "Development Tools",
       description: "Optimizing workflows with modern development and build tools.",
-      subicons: "icon"
+      subicons: ["fa-brands fa-npm", "fa-brands fa-node-js", "fa-brands fa-prettier"],
+      SubiconsName: ["npm", "Node.js", "Prettier"]
     },
     {
       icon: "fa-brands fa-git-alt",
       title: "Version Control & Deployment",
       description: "Managing code collaboration and deploying applications efficiently.",
-      subicons: "icon"
+      subicons: ["fa-brands fa-git", "fa-brands fa-github", "fa-brands fa-vercel"],
+      SubiconsName: ["Git", "GitHub", "Vercel"]
     },
   ]
   return (
@@ -59,14 +65,19 @@ const Skills = () => {
             viewport={{ once: false, amount: 0.3 }}
             className="block p-4 border border-gray-200 rounded-lg shadow shadow-lg bg-gray-800 border-gray-700 hover:bg-teal-300/20"
           >
-            <h1 className="pb-6 mr-2"><i className={`text-2xl text-teal-300 ${skill.icon}`}></i></h1>
-            <h5 className="mb-2 text-center text-xl font-bold tracking-tight text-gray-900 text-white">
+            <h1 className="py-1 mr-2"><i className={`text-2xl text-teal-300 ${skill.icon}`}></i></h1>
+            <h5 className="text-center text-xl font-bold tracking-tight text-gray-900 text-white">
             {skill.title}
             </h5>
-            <p className="font-normal text-sm text-center py-2 text-gray-400">
+            <p className="font-normal text-sm text-center py-1 text-gray-400">
             {skill.description}
             </p>
-            <p className="font-normal  text-gray-400">{skill.subicons}</p>
+           {/* sub icons */}
+            <div className="flex flex-wrap  w-full mt-4">
+              {skill.subicons.map((sub, i) => (
+                <span key={i} className="flex text-sm items-center mr-2 px-2 py-1 rounded-md mb-2 text-white bg-teal-300/10"><i className={`text-sm text-teal-300 pr-1 ${sub}`}></i>{skill.SubiconsName[i]}</span>
+              ))}
+            </div>
           </motion.div>
         ))}
       </div>
